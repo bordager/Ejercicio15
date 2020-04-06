e4e297107b394ecfdfd68774b340d6616c661717
bordager <63132719+bordager@users.noreply.github.com>
2020-04-06 19:21:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre de usuario");
+            string usuario;
+            usuario = Console.ReadLine();
+            string pass;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Introduzca su password");
+                pass = Console.ReadLine();
+                if (usuario == pass)
+                {
+                    Console.WriteLine("Bienvenido " + usuario);
+                    Console.ReadLine();
+                        break;
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña Incorrecta");
+                    if (i == 2)
+                    {
+                        Console.WriteLine("Usuario Bloqueado");
+                        Console.ReadLine();
+                        
+                    }
+                }
+            
+            }
+
+
+
+        }
+    }
+}
